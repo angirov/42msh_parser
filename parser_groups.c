907a7bf4d579ce77c7ee3bbbf7625832082b6bd4
Vladimir Angirov <angirov@posteo.net>
2022-08-24 19:23:36 +0200

    change dealing with ||,&&,| at the edges

--- parser_groups.c
+++ parser_groups.c
@@ -6,11 +6,56 @@
 /*   By: vangirov <vangirov@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 06:32:00 by vangirov          #+#    #+#             */
-/*   Updated: 2022/07/13 19:37:54 by vangirov         ###   ########.fr       */
+/*   Updated: 2022/08/24 19:20:36 by vangirov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void ft_trim_lexems_front(t_list **lexems)
+{
+	t_list	*link;
+	t_list	*next;
+	int		type;
+
+	link = *lexems;
+	while(link)
+	{
+		next = link->next;
+		type = ft_ectracttype(link);
+		if (type == LX_AND || type == LX_OR || type == LX_SEP || type == LX_PIPE)
+		{
+			ft_lst_detach_from(lexems, link);
+			ft_free_lexem(link);
+		}
+		else
+			break;
+		link = next;
+	}
+}
+
+void ft_trim_lexems_back(t_list **lexems)
+{
+	t_list	*link;
+	t_list	*prev;
+	int		type;
+
+	link = ft_lstlast(*lexems);
+	while(link)
+	{
+		prev = ft_lst_find_preceding(lexems, link);
+		type = ft_ectracttype(link);
+		if (type == LX_AND || type == LX_OR || type == LX_SEP || type == LX_PIPE)
+		{
+			ft_lst_detach_from(lexems, link);
+			ft_free_lexem(link);
+		}
+		else
+			break;
+		link = prev;
+	}
+}
+
 /* if the first token is AND or OR print error */
 int	ft_count_groups(t_list **lexems)
 {
@@ -18,16 +63,14 @@ int	ft_count_groups(t_list **lexems)
 	int		type;
 	t_list	*link;
 
+	ft_trim_lexems_front(lexems);
+	ft_trim_lexems_back(lexems);
 	if (!*lexems)
 		return (0);
 	counter = 1;
 	link = *lexems;
 	type = ft_ectracttype(link);
-	if (type == LX_AND || type == LX_OR)
-		return (ft_error("syntax error: || or && as the first token", -301));
-	type = ft_ectracttype(ft_lstlast(link));
-	if (type == LX_AND || type == LX_OR)
-		return (ft_error("syntax error: || or && as the last token", -399));
+
 	while (link)
 	{
 		type = ft_ectracttype(link);
@@ -38,6 +81,18 @@ int	ft_count_groups(t_list **lexems)
 	return (counter);
 }
 
+void	ft_init_group(t_msh *msh, int i, int type)
+{
+	msh->groups[i] = malloc(sizeof(t_group));
+	msh->groups[i]->index = i;
+	msh->groups[i]->lexems = malloc(sizeof(t_list *));
+	*msh->groups[i]->lexems = NULL;
+	if (i == 0)
+		msh->groups[i]->type = -1;
+	else
+		msh->groups[i]->type = type;
+}
+
 int	ft_makegroups(t_msh *msh)
 {
 	int		i;
@@ -46,21 +101,18 @@ int	ft_makegroups(t_msh *msh)
 	char	*text;
 
 	msh->group_num = ft_count_groups(msh->lexems);
+	printf("gr num: %d\n", msh->group_num);
 	if (msh->group_num <= 0)
+	{
+		ft_free_lexems(msh->lexems);
 		return (ft_error("could not parse", -300));
+	}
 	msh->groups = malloc(sizeof(t_group *) * msh->group_num);
 	i = 0;
 	link = *msh->lexems;
 	while (i < msh->group_num)
 	{
-		msh->groups[i] = malloc(sizeof(t_group));
-		msh->groups[i]->index = i;
-		msh->groups[i]->lexems = malloc(sizeof(t_list *));
-		*msh->groups[i]->lexems = NULL;
-		if (i == 0)
-			msh->groups[i]->type = -1;
-		else
-			msh->groups[i]->type = ft_ectracttype(link);
+		ft_init_group(msh, i, ft_ectracttype(link));
 		while (link)
 		{
 			type = ft_ectracttype(link);
@@ -73,5 +125,5 @@ int	ft_makegroups(t_msh *msh)
 		ft_dellastsep(msh->groups[i]->lexems);
 		i++;
 	}
-	return (0);
+	return (msh->group_num);
 }
