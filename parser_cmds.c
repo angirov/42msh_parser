a067f8683fda48a892155414c7266bd9c5262d18
Vladimir Angirov <angirov@posteo.net>
2022-08-25 15:13:02 +0200

    re-refactor ft_make_cmd_args

--- parser_cmds.c
+++ parser_cmds.c
@@ -6,7 +6,7 @@
 /*   By: vangirov <vangirov@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 11:33:00 by vangirov          #+#    #+#             */
-/*   Updated: 2022/08/24 23:52:20 by vangirov         ###   ########.fr       */
+/*   Updated: 2022/08/25 15:10:11 by vangirov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,31 +36,37 @@ int	ft_count_cmds(t_list **lexems)
 	return (counter);
 }
 
-void	ft_loop_thru_args(t_list *link, t_group *group, int i)
+void	ft_make_cmd_args_2(t_group *group, t_list *link)
 {
+	int		i;
 	int		type;
 	char	*text;
 
-	while (link)
+	i = 0;
+	while (i < group->cmds->cmd_num)
 	{
-		type = ft_ectracttype(link);
-		if (type == LX_PIPE && *group->cmds->cmd_args[i])
+		group->cmds->cmd_args[i] = ft_calloc(1, sizeof(t_list *));
+		while (link)
 		{
+			type = ft_ectracttype(link);
+			if (type == LX_PIPE && *group->cmds->cmd_args[i])
+			{
+				link = link->next;
+				break ;
+			}
+			if (!(type == LX_SEP && !*group->cmds->cmd_args[i]))
+			{
+				text = ft_strdup(ft_ectracttext(link));
+				ft_addlexem(group->cmds->cmd_args[i], ft_newlexem(type, text));
+			}
 			link = link->next;
-			break ;
 		}
-		if (!(type == LX_SEP && !*group->cmds->cmd_args[i]))
-		{
-			text = ft_strdup(ft_ectracttext(link));
-			ft_addlexem(group->cmds->cmd_args[i], ft_newlexem(type, text));
-		}
-		link = link->next;
+		ft_dellastsep(group->cmds->cmd_args[i++]);
 	}
 }
 
 int	ft_make_cmd_args(t_group *group)
 {
-	int		i;
 	t_list	*link;
 
 	group->cmds = malloc(sizeof(t_cmds));
@@ -68,15 +74,7 @@ int	ft_make_cmd_args(t_group *group)
 	if (group->cmds->cmd_num <= 0)
 		return (ft_error("could not parse group", -400 -(10 * group->index)));
 	group->cmds->cmd_args = malloc(sizeof(t_list **) * group->cmds->cmd_num);
-	i = 0;
 	link = *group->lexems;
-	while (i < group->cmds->cmd_num)
-	{
-		group->cmds->cmd_args[i] = malloc(sizeof(t_list *));
-		*group->cmds->cmd_args[i] = NULL;
-		ft_loop_thru_args(link, group, i);
-		ft_dellastsep(group->cmds->cmd_args[i]);
-		i++;
-	}
+	ft_make_cmd_args_2(group, link);
 	return (0);
 }
